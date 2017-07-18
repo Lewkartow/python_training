70a85b2dbdfc9e9067f07d3b1274047c9704cf21
Lewkartow <lyzin.alex@gmail.com>
2017-07-18 22:00:12 +0300

    Created class for manage the sessions

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,21 +7,12 @@ class Application:
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self, driver):
         driver = self.driver
         driver.get("http://localhost/addressbook/")
 
-    def login(self, username, password):
-        driver = self.driver
-        self.open_home_page(driver)
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self):
         driver = self.driver
@@ -45,9 +37,6 @@ class Application:
         driver = self.driver
         driver.find_element_by_link_text("groups").click()
 
-    def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.driver.quit()
