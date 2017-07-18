dc6d3442614ac979620b8a4c82624887bb24a2a7
Lewkartow <lyzin.alex@gmail.com>
2017-07-18 16:06:36 +0300

    Introducing parameters

--- test_add_group.py
+++ test_add_group.py
@@ -11,25 +11,25 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, driver):
           driver.get("http://localhost/addressbook/")
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
-    def create_group(self, driver):
+    def create_group(self, driver, name, header, footer):
         # fill group form
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("ABC")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("BCA")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("ZXC")
+        driver.find_element_by_name("group_footer").send_keys(footer)
         driver.find_element_by_name("submit").click()
 
     def return_to_groups_page(self, driver):
@@ -43,9 +43,9 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_groups_page(driver)
-        self.create_group(driver)
+        self.create_group(driver, name="ABC", header="BCA", footer="ZXC")
         self.return_to_groups_page(driver)
         self.logout(driver)
 
