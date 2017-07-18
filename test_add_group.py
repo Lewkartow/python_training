5560e87f9e9940263b22f5bcbf1237b781fb4bc4
Lewkartow <lyzin.alex@gmail.com>
2017-07-18 16:34:14 +0300

    Work stability with unitest
    PyTest does not work

--- test_add_group.py
+++ test_add_group.py
@@ -9,9 +9,11 @@ class TestAddGroup(unittest.TestCase):
         self.driver.implicitly_wait(30)
 
     def open_home_page(self, driver):
-          driver.get("http://localhost/addressbook/")
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
 
-    def login(self, driver, username, password):
+    def login(self, username, password):
+        driver = self.driver
         self.open_home_page(driver)
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
@@ -19,11 +21,13 @@ class TestAddGroup(unittest.TestCase):
         driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def open_groups_page(self, driver):
+    def open_groups_page(self):
+        driver = self.driver
         driver.find_element_by_link_text("groups").click()
 
-    def create_group(self, driver, name, header, footer):
-        self.open_groups_page(driver)
+    def create_group(self, name, header, footer):
+        driver = self.driver
+        self.open_groups_page()
         # fill group form
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").clear()
@@ -33,26 +37,26 @@ class TestAddGroup(unittest.TestCase):
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys(footer)
         driver.find_element_by_name("submit").click()
-        self.return_to_groups_page(driver)
+        self.return_to_groups_page()
 
-    def return_to_groups_page(self, driver):
+    def return_to_groups_page(self):
+        driver = self.driver
         driver.find_element_by_link_text("groups").click()
 
-    def logout(self, driver):
+    def logout(self):
         # logout
+        driver = self.driver
         driver.find_element_by_link_text("Logout").click()
 
     def test_add_group(self):
-        driver = self.driver
-        self.login(driver, username="admin", password="secret")
-        self.create_group(driver, name="ABC", header="BCA", footer="ZXC")
-        self.logout(driver)
+        self.login(username="admin", password="secret")
+        self.create_group(name="ABC", header="BCA", footer="ZXC")
+        self.logout()
 
     def test_add_empty_group(self):
-        driver = self.driver
-        self.login(driver, username="admin", password="secret")
-        self.create_group(driver, name="", header="", footer="")
-        self.logout(driver)
+        self.login(username="admin", password="secret")
+        self.create_group(name="", header="", footer="")
+        self.logout()
 
     def tearDown(self):
         self.driver.quit()
