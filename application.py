f5be13ad8266cb0768e4a96d3a0d71773dbd076e
Lewkartow <lyzin.alex@gmail.com>
2017-07-18 16:41:27 +0300

    Missed lesson. Stability

---
+++ application.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import unittest
+
+class Aplication:
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+
+    def open_home_page(self, driver):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        driver = self.driver
+        self.open_home_page(driver)
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_groups_page(self):
+        driver = self.driver
+        driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, name, header, footer):
+        driver = self.driver
+        self.open_groups_page()
+        # fill group form
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(name)
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(header)
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(footer)
+        driver.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        driver = self.driver
+        driver.find_element_by_link_text("groups").click()
+
+    def logout(self):
+        # logout
+        driver = self.driver
+        driver.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.driver.quit()
