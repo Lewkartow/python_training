4f9a229e17ba9554f31bfa836c39b994c9310e9b
Lewkartow <lyzin.alex@gmail.com>
2017-07-19 15:10:10 +0300

    Fixed without duct tape

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,7 +9,6 @@ def test_add_group(app):
     app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.logout()
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name=" ", header=" ", footer=" "))
     app.session.logout()
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,6 @@ __author__ = 'Алексей'
 
 
 def test_delete_first_group(app):
-    app.session.logout()
     app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
